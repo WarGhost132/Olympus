c4db39ab5545077d8edba1985d6e7a4d47b9dae2
WarGhost132 <wawghostick@gmail.com>
2024-08-29 13:42:41 +0300

    Add code

---
+++ pages/white/index.php
@@ -0,0 +1,173 @@
+<?php
+
+// Head
+$PAGE_TITLE = "OLYMPUS";
+$PAGE_DESCRIPTION = "Description";
+
+// Content below
+?>
+
+<main>
+    <section class="section main__section">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-md-6 d-flex align-items-center px-0">
+                    <div class="main__content text-start">
+                        <div data-aos="zoom-in-up" class="main__title__wrapper d-flex justify-content-start align-items-center">
+                            <h1 class="px-3 px-sm-5 pt-3">Olympus</h1>
+                        </div>
+                        <div class="sticks__wrapper d-none d-sm-flex gap-3 px-5 py-5">
+                            <div class="stick first"></div>
+                            <div class="stick second"></div>
+                            <div class="stick third"></div>
+                        </div>
+                        <div class="main__content__wrapper px-3 px-sm-5 py-0 py-sm-5">
+                            <p data-aos="fade-up-right">Добро пожаловать в Olympus, игру, которая приглашает вас в мифический мир древнегреческих богов и героев.</p>
+                            <br>
+                            <p data-aos="fade-up-right">Это не просто игра — это эпическое приключение, где каждый шаг ведет вас к новым открытиям и захватывающим победам.</p>
+                            <br>
+                            <p data-aos="fade-up-right">Приготовьтесь покорить Олимп и стать настоящим героем!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 d-none d-md-block px-0">
+                    <div data-aos="fade-up" class="main__image__wrapper d-flex align-items-start justify-content-center">
+                        <img src="assets/images/main.webp" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section advantages__section position-relative px-0 pt-0">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 px-0">
+                    <div class="advantages__title__wrapper d-flex py-3 justify-content-center justify-content-sm-end">
+                        <div class="advantages__title__block">
+                            <h2 data-aos="fade-up" class="text-center text-sm-start mb-0">Четыре причины отправиться на покорение Olympus:</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 px-0">
+                    <div class="advantages__content__wrapper d-flex flex-column align-items-end py-5">
+                        <div class="advantages__content right text-start text-md-end">
+                            <p data-aos="fade-down" class="subtitle text-center text-md-end">Божественные выигрыши ждут вас</p>
+                            <p data-aos="fade-up">Сразитесь с богами и завоюйте свою долю небесных богатств! </p>
+                            <br>
+                            <p data-aos="fade-up">Olympus предлагает игрокам уникальные возможности выигрыша, которые способны удивить даже самых опытных азартных игроков. Каждое вращение барабана приближает вас к заветному джекпоту.</p>
+                        </div>
+                    </div>
+                    <div class="sticks__wrapper d-none d-sm-flex gap-3 justify-content-center justify-content-md-end helper__one__wrapper">
+                        <div class="stick first white"></div>
+                        <div class="stick second"></div>
+                        <div class="stick third"></div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-12 px-0">
+                    <div class="advantages__image__wrapper right__wrapper">
+                        <div class="advantages__back__image position-relative">
+                            <div class="advantages__image d-block d-md-none">
+                                <img data-aos="zoom-in" class="first" src="assets/images/advantages_1.webp" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 px-0">
+                    <div class="advantages__content__wrapper d-flex flex-column justify-content-center align-items-start py-5">
+                        <div class="advantages__content left text-start text-md-start">
+                            <p data-aos="fade-down" class="subtitle text-center text-md-start">Восхитительное приключение</p>
+                            <p data-aos="fade-up">Почувствуйте себя героем древнегреческих мифов! Каждый раунд в Olympus — это новое приключение, полное неожиданностей и возможностей.</p>
+                            <br>
+                            <p data-aos="fade-up">Погрузитесь в мир легенд, где каждое действие может стать решающим.</p>
+                        </div>
+                    </div>
+                    <div class="sticks__wrapper d-none d-sm-flex gap-3 justify-content-center justify-content-md-start align-items-end helper__two__wrapper">
+                        <div class="stick first white"></div>
+                        <div class="stick second"></div>
+                        <div class="stick third"></div>
+                    </div>
+                </div>
+                <div class="col-12 px-0">
+                    <div class="advantages__image__wrapper right__wrapper">
+                        <div class="advantages__back__image position-relative">
+                            <div class="advantages__image d-block d-md-none">
+                                <img data-aos="zoom-in" class="second" style="object-fit: cover;" src="assets/images/advantages_2.webp" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 px-0">
+                    <div class="advantages__content__wrapper d-flex flex-column justify-content-center align-items-end py-5">
+                        <div class="advantages__content right text-start text-md-end">
+                            <p data-aos="fade-down" class="subtitle text-center text-md-end">Грандиозные визуальные эффекты</p>
+                            <p data-aos="fade-up">Насладитесь величественной красотой древней Греции с потрясающей графикой и анимацией.</p>
+                            <br>
+                            <p data-aos="fade-up">Olympus оживает на экране, передавая атмосферу мифического мира и создавая эффект полного присутствия среди богов.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 px-0">
+                    <div class="advantages__image__wrapper left__wrapper">
+                        <div class="advantages__back__image position-relative">
+                            <div class="advantages__image d-block d-md-none">
+                                <img data-aos="zoom-in" class="third" src="assets/images/advantages_3.webp" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 px-0">
+                    <div class="advantages__content__wrapper d-flex flex-column justify-content-center align-items-end py-5">
+                        <div class="advantages__content right text-start text-md-end">
+                            <p data-aos="fade-down" class="subtitle text-center text-md-end">Подходит для всех</p>
+                            <p data-aos="fade-up">Независимо от того, новичок вы или опытный игрок, Olympus предложит вам интересные механики и множество возможностей для победы.</p>
+                            <br>
+                            <p data-aos="fade-up">Простые правила и захватывающий геймплей делают игру доступной для всех желающих испытать свои силы.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Olympys image -->
+        <div class="olympus-advantages-image-wrapper position-absolute">
+            <div class="position-relative">
+                <!-- Advantages-1 -->
+                <div class="position-absolute advantages-1-wrapper d-flex justify-content-center align-items-center">
+                    <div class="back_part_advantages_1 position-absolute"></div>
+                    <div class="advantages-1-image"></div>
+                </div>
+                <!-- Advantages-2 -->
+                <div class="position-absolute advantages-2-wrapper d-flex justify-content-center align-items-center">
+                    <div class="back_part_advantages_2 position-absolute"></div>
+                    <div class="advantages-2-image"></div>
+                </div>
+                <!-- Advantages-3 -->
+                <div class="position-absolute advantages-3-wrapper d-flex justify-content-center align-items-center">
+                    <div class="back_part_advantages_3 position-absolute"></div>
+                    <div class="advantages-3-image"></div>
+                </div>
+                <!-- Back-1 -->
+                <div class="position-absolute back-1-wrapper">
+                    <div class="back-1-image"></div>
+                </div>
+                <!-- Back-2 -->
+                <div class="position-absolute back-2-wrapper">
+                    <div class="back-2-image"></div>
+                </div>
+                <!-- Back-3 -->
+                <div class="position-absolute back-3-wrapper">
+                    <div class="back-3-image"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section games__section">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center py-5">
+                    <button data-aos="fade-right" class="p-1 px-5">Попробовать сейчас<span class="flare"></span></button>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
